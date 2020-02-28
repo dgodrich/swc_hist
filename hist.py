58ace0267dfb10b78f121f7f8fa871e2e1eaecd1
Dana Godrich <d.godrich@miami.edu>
2020-02-28 10:37:47 -0500

    Final script?

--- hist.py
+++ hist.py
@@ -1,6 +1,7 @@
 # Randomizing data, generating summary stats + histogram
 
 import numpy as np
+import matplotlib.pyplot as plt 
 
 mu = 80
 sigma = 10
@@ -10,3 +11,8 @@ x = np.random.normal(mu, sigma, 100)
 print ("Random Norm Array Mean Centered:",x[:10])
 
 print ("mean", np.mean(x))
+
+plt.hist(x)
+plt.show()
+
+print ("I am a champion of science")
